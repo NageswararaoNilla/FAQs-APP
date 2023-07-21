1b460b63abc55d776b42e71876a51daa57e82995
NageswararaoNilla <nillanageswararao075@gmail.com>
2023-07-21 15:22:40 +0000

    project completed

--- src/components/FaqItem/index.js
+++ src/components/FaqItem/index.js
@@ -1,1 +1,39 @@
-// Write your code here.
+import './index.css'
+
+const FaqItem = props => {
+  const {faqDetails, showAndHideIcon} = props
+  const {id, questionText, answerText, isVisible} = faqDetails
+
+  const onClickIcon = () => {
+    showAndHideIcon(id)
+  }
+
+  const imgUrl = isVisible
+    ? 'https://assets.ccbp.in/frontend/react-js/faqs-minus-icon-img.png'
+    : 'https://assets.ccbp.in/frontend/react-js/faqs-plus-icon-img.png'
+
+  const renderAnswer = () => (
+    <div>
+      <hr className="line" />
+      <p className="answer">{answerText}</p>
+    </div>
+  )
+
+  return (
+    <li className="question-answer-container">
+      <div className="question-container">
+        <h1 className="question">{questionText}</h1>
+        <button type="button" onClick={onClickIcon} className="btn">
+          <img
+            src={imgUrl}
+            alt={isVisible ? 'minus' : 'plus'}
+            className="plus-icon"
+          />
+        </button>
+      </div>
+      {isVisible && renderAnswer()}
+    </li>
+  )
+}
+
+export default FaqItem
--- src/components/Faqs/index.js
+++ src/components/Faqs/index.js
@@ -1,1 +1,50 @@
-// Write your code here.
+import {Component} from 'react'
+
+import FaqItem from '../FaqItem'
+
+import './index.css'
+
+class Faqs extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      faqsList: props.faqsList.map(each => ({...each, isVisible: false})),
+    }
+  }
+
+  showAndHideIcon = id => {
+    const {faqsList} = this.state
+    const updateList = faqsList.map(each => {
+      if (each.id === id) {
+        return {...each, isVisible: !each.isVisible}
+      }
+      return each
+    })
+    this.setState({
+      faqsList: updateList,
+    })
+  }
+
+  render() {
+    const {faqsList} = this.state
+    console.log(faqsList)
+    return (
+      <div className="app-container">
+        <div className="faqs-container">
+          <h1 className="heading">FAQs</h1>
+          <ul>
+            {faqsList.map(eachFaq => (
+              <FaqItem
+                key={eachFaq.id}
+                faqDetails={eachFaq}
+                showAndHideIcon={this.showAndHideIcon}
+              />
+            ))}
+          </ul>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Faqs
